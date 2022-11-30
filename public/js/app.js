9bd82322344045fedd2248e5fe85ef4f3e9fb722
YOBAMALABAMA <viraj.rokade21@gmail.com>
2022-11-30 11:38:51 +0530

    farmhelp dro

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+   
+const li = document.querySelectorAll(".links");
+const sec = document.querySelectorAll("section");
+
+function activeLink(){
+    let len=sec.length;
+    while(--len && window.scrollY + 97 < sec[len].offsetTop){}
+    li.forEach(ltx => ltx.classList.remove("active"))
+    li[len].classList.add("active");
+    }
+
+activeLink();
+window.addEventListener("scroll",activeLink)
